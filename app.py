85e357afac03e0b1a0c788787ea67ff6c2338360
Toripppppy <Lundy11@icloud.com>
2023-01-27 14:01:14 +0900

    tokenを環境変数に移動

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import degreer
 
 # ボットトークンと署名シークレットでアプリを初期化
 app = App(
-    token='xoxb-4658828931218-4658721256579-g7JxcYfcQG2BDYe0rCN6kMdX',
-    signing_secret='ea138abb01c0b890196f978fe0d5b72f'
+    token = os.getenv('BOT_TOKEN'),
+    signing_secret = os.getenv('BOT_SECRET')
 )
 
 
